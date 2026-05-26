db2408f6671effdd320d92f5ac3bdfa72d163d92
vinayg2509 <vinayg2509@gmail.com>
2026-05-26 21:12:26 +0530

    [Vinay]Added /api in BookController and GenreController @RequestMapping

--- src/main/java/com/vinay/controller/BookController.java
+++ src/main/java/com/vinay/controller/BookController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
--- src/main/java/com/vinay/controller/GenreController.java
+++ src/main/java/com/vinay/controller/GenreController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/genre")
+@RequestMapping("/api/genre")
 @RequiredArgsConstructor
 public class GenreController {
 
